5c129dee601998125f977424086337fb824a6d98
youngtaekiim <youngtae88.kim@lge.com>
2026-03-26 04:28:03 -0400

    pullpiri yaml

--- nuc_base/nuc_guest/README.md
+++ nuc_base/nuc_guest/README.md
@@ -5,3 +5,11 @@ x86_64 아키텍처의 CentOS Stream 사용하는 NUC 에서 진행
 ## Arduino 설치
 
 [arduino](./arduino/README.md) 참조
+
+## Docker image
+
+In `kuksa-serial-bridge` folder,
+
+```
+sudo podman build -t localhost/kuksa-serial-bridge:latest
+```
---
+++ pullpiri-yaml/launch-passive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright 2024 LG Electronics Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+BODY=$(< ./yaml/buzzer-passive-launch.yaml)
+
+curl -X POST 'http://192.168.1.2:47099/api/artifact' \
+--header 'Content-Type: text/plain' \
+--data "${BODY}"
