658b03cb47c5f6c02a20e4c9935c7bc2b6403200
youngtaekiim <youngtae88.kim@lge.com>
2026-03-25 23:00:11 -0400

    abstract buzzer

--- nuc_base/nuc_guest/kuksa-serial-bridge/bridge.py
+++ nuc_base/nuc_guest/kuksa-serial-bridge/bridge.py
@@ -7,8 +7,9 @@ import time
 from kuksa_client import KuksaClientThread
 from kuksa_client.grpc import Datapoint
 
-PORT_ACTIVE = "/dev/arduino_active"
-PORT_PASSIVE = "/dev/arduino_passive"
+#PORT_ACTIVE = "/dev/arduino_active"
+#PORT_PASSIVE = "/dev/arduino_passive"
+PORT_BUZZER = "/dev/arduino_buzzer"
 BAUD = 115200
 
 def recv_to_databroker(client):
@@ -40,17 +41,20 @@ def main():
     #client = KuksaClientThread(config={'url': 'grpc://192.168.1.2:55555', 'insecure': True})
     client.start()
 
-    with serial.Serial(PORT_ACTIVE, BAUD, timeout=0) as s_ac, \
-         serial.Serial(PORT_PASSIVE, BAUD, timeout=0) as s_pa:
+    #with serial.Serial(PORT_ACTIVE, BAUD, timeout=0) as s_ac, \
+    #     serial.Serial(PORT_PASSIVE, BAUD, timeout=0) as s_pa:
+    with serial.Serial(PORT_BUZZER, BAUD, timeout=0) as s_bz:
         while True:
             isBrake = recv_to_databroker(client)
             print(f"isBrake: {isBrake}")
 
             msg = b"1" if isBrake else b"0"
-            s_ac.write(msg)
-            s_pa.write(msg)
-            s_ac.flush()
-            s_pa.flush()
+            #s_ac.write(msg)
+            #s_pa.write(msg)
+            #s_ac.flush()
+            #s_pa.flush()
+            s_bz.write(msg)
+            s_bz.flush()
 
             time.sleep(0.1)
 
